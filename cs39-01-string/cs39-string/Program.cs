e65e1a80ba530f77963b651c5dc5122cba88b9eb
Alan L <aluong91@yahoo.com>
2011-09-10 14:06:44 -0700

    first commit

---
+++ cs39-01-string/cs39-string/Program.cs
@@ -0,0 +1,20 @@
+﻿// String Input Output
+// Assignment #1
+// Print the user's name and system time
+// By Alan Luong
+// ID #832899
+
+class StringIO
+{
+    static void Main()
+    {
+
+        System.Console.WriteLine("Alan Luong");
+        System.Console.WriteLine("{0}\n", System.DateTime.Now);
+
+        System.Console.Write("What is your name? ");
+        string username = System.Console.ReadLine();
+
+        System.Console.WriteLine("Hi {0}, I'm Alan. Get lost.", username);
+    }
+}
---
+++ cs39-03-assignmentvariables/cs39-03-assignmentvariables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cs39_03_assignmentvariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] input =  new float[10];
+            float sum = 0;
+
+            for (int i = 0; i < input.Length; i++) {
+                System.Console.Out.Write("Enter number {0}: ", i + 1);
+                input[i] = float.Parse(System.Console.ReadLine());
+                sum += input[i];
+            }
+
+            Array.Sort(input);
+            System.Console.Out.Write("\nYou entered");
+            for (int i = 0; i < input.Length; i++)
+                System.Console.Out.Write(" {0}", input[i]);
+
+            System.Console.Out.WriteLine("\nThe average is {0}", sum / input.Length);
+            System.Console.Out.WriteLine("The median is {0}", input[4]);
+            System.Console.Out.WriteLine("The sum is {0}", sum);
+        }
+    }
+}
